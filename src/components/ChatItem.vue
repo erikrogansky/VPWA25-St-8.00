679d1e1e152c04be9a9f612a73fd6203848e7df6
Erik Roganský <roganskyerik@gmail.com>
2024-10-05 12:42:48 +0200

    ChatItem hover fix

--- src/components/ChatItem.vue
+++ src/components/ChatItem.vue
@@ -1,22 +1,38 @@
 <template>
-    <q-item class="chat-item">
-      <q-item-section avatar>
-        <i class="fas fa-circle-user"></i>
-      </q-item-section>
-      <q-item-section>
-        <div>
-          <q-item-label class="chat-name">Chat name</q-item-label>
-        </div>
-        <div>
-          <q-item-label class="chat-message">Last message</q-item-label>
-        </div>
-      </q-item-section>
-      <div class="hover-icon">
+  <q-item class="chat-item" @mouseover="hover" @mouseleave="unhover">
+    <q-item-section avatar>
+      <i class="fas fa-circle-user"></i>
+    </q-item-section>
+    <q-item-section>
+      <div class="chat-item-content">
+        <q-item-label class="chat-name">Chat name</q-item-label>
+        <q-item-label class="chat-message">Last message</q-item-label>
+      </div>
+    </q-item-section>
+    <transition name="fade" appear>
+      <div v-show="show" class="hover-icon">
         <i class="fas fa-ellipsis"></i>
       </div>
-    </q-item>
+    </transition>
+  </q-item>
 </template>
 
-<script setup scoped lang="ts">
+<script setup lang="ts">
+import { ref } from 'vue';
 
+const show = ref(false);
+const hovering = ref(false);
+
+
+const hover = () => {
+  hovering.value = true;
+  setTimeout(() => {
+    show.value = hovering.value;
+  }, 200);
+};
+
+const unhover = () => {
+  hovering.value = false;
+  show.value = false;
+};
 </script>
