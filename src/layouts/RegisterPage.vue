d988ae5c74c1200318616f961a3863dea1e98965
Erik Roganský <roganskyerik@gmail.com>
2024-10-09 09:12:52 +0200

    Registration page fix

--- src/layouts/RegisterPage.vue
+++ src/layouts/RegisterPage.vue
@@ -1,61 +1,3 @@
-<!--<template>
-  <q-layout view="hHh p fFf">
-
-      <q-page-container>
-          <q-card flat class="q-pa-md">
-
-            <q-card-section class="text-center">
-              <img src="../assets/logo.png" alt="Convo's logo">
-              <div class="text-h5 text-weight-bold q-pa-sm">Create an Account</div>
-
-            </q-card-section>
-
-            <q-card-section>
-              <q-input dense outlined rounded v-model="email" label="Email Address"></q-input>
-              <q-input dense outlined rounded v-model="password" type="password" label="Password" class="q-mt-md"></q-input>
-              <q-input dense outlined rounded v-model="email" label="Email Address" class="q-mt-md"></q-input>
-              <q-input dense outlined rounded v-model="email" label="Email Address" class="q-mt-md"></q-input>
-              <q-input dense outlined rounded v-model="email" label="Email Address" class="q-mt-md"></q-input>
-              <q-input dense outlined rounded v-model="email" label="Email Address" class="q-mt-md"></q-input>
-            </q-card-section>
-
-            <q-card-section>
-              <q-btn unelevated rounded label="Sign Up" to="/app" no-caps class="login-btn" />
-            </q-card-section>
-
-            <q-card-section class="text-center q-pt-none">
-              <div class="text-8">
-                Already have an account?
-                <a href="#">Sign in.</a>
-              </div>
-            </q-card-section>
-
-          </q-card>
-
-      </q-page-container>
-
-    <FooterLayout/>
-
-  </q-layout>
-
-</template>
-
-<script scoped setup lang="ts">
-import { ref } from 'vue';
-import FooterLayout from 'src/components/FooterLayout.vue';
-
-const nick_name = ref<string>('');
-const first_name = ref<string>('');
-const last_name = ref<string>('');
-const date_of_birth = ref<string>('');
-const gender = ref<string>('');
-
-const phone_number = ref<string>('');
-const email = ref<string>('');
-const password = ref<string>('');
-
-</script>-->
-
 <template>
   <q-layout view="hHh p fFf">
     <q-page-container>
@@ -70,20 +12,31 @@ const password = ref<string>('');
           <div class="q-gutter">
             <q-input dense outlined rounded v-model="nick_name" placeholder="Nickname"/>
             <div class="q-gutter q-mt-md row">
-              <q-input dense outlined rounded v-model="first_name" placeholder="First Name" class="col"/>
-              <q-input dense outlined rounded v-model="last_name" placeholder="Last Name" class="col"/>
+              <div class="name"><q-input dense outlined rounded v-model="first_name" placeholder="First Name" class="col"/></div>
+              <div class="name"><q-input dense outlined rounded v-model="last_name" placeholder="Last Name" class="col"/></div>
             </div>
             <div class="q-gutter q-mt-md row">
-              <q-input dense outlined rounded v-model="date_of_birth" type="date" label="Date of Birth" class="col"/>
-              <q-input dense outlined rounded v-model="gender" label="Gender" class="col"/>
+              <div class="date_gender">
+                  <div style="max-width: 300px">
+                    <q-input dense outlined rounded v-model="date_of_birth" mask="date" placeholder="Date of birth">
+                      <template v-slot:append>
+                        <q-icon name="event" class="cursor-pointer">
+                          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                            <q-date dark v-model="date_of_birth">
+                              <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                              </div>
+                            </q-date>
+                          </q-popup-proxy>
+                        </q-icon>
+                      </template>
+                    </q-input>
+                  </div>
+              </div>
+              <div class="date_gender"><q-select dense outlined rounded v-model="gender" label="Gender" class="col" :options="options" /></div>
             </div>
-
           </div>
           <div class="">
-            <!--<div class="q-gutter-md row">
-              <q-input dense outlined rounded v-model="date_of_birth" label="Date of Birth" class="col" type="date"/>
-              <q-select dense outlined rounded v-model="gender" label="Gender" :options="genderOptions" class="col"/>
-            </div>-->
             <q-input dense outlined rounded v-model="phone_number" placeholder="Phone Number" class="q-mt-md"/>
             <q-input dense outlined rounded v-model="email" placeholder="Email Address" class="q-mt-md"/>
             <q-input dense outlined rounded v-model="password" type="password" placeholder="Password" class="q-mt-md"/>
@@ -120,10 +73,12 @@ const nick_name = ref<string>('');
 const first_name = ref<string>('');
 const last_name = ref<string>('');
 const date_of_birth = ref<string>('');
-const gender = ref<string>('');
+const gender = ref<string | null>(null)
 const phone_number = ref<string>('');
 const email = ref<string>('');
 const password = ref<string>('');
+
+const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle']
 
 </script>
 
@@ -167,6 +122,29 @@ const password = ref<string>('');
     margin-right: 10px;
     padding-left: 20px;
     padding-right: 20px;
+  }
+
+  .date_gender {
+    display: flex;
+    flex-direction: column;
+    width: 48%;
+
+    .label {
+      padding-left: 8px;
+      padding-bottom: 6px;
+    }
+  }
+
+  .q-gutter.q-mt-md.row, .q-gutter.q-mt-sm.row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .name {
+    display: flex;
+    flex-direction: column;
+    width: 48%;
   }
 
 </style>
