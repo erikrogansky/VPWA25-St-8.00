63ade5c100f7463d6edea6c26db835e1ba27e76a
Peter Sartoris <petersartoris1@gmail.com>
2024-10-18 22:07:28 +0200

    creating AboutPage

---
+++ src/layouts/AboutPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="hhh lpr fff">
+
+    <HeaderLayout/>
+
+    <q-page-container class="content">
+
+      <h1 id="whats-new">What's new</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tortor fermentum, egestas sapien vitae, finibus elit. Fusce ullamcorper nec massa vel eleifend. Suspendisse luctus vitae orci sit amet maximus. Donec dictum ligula non dapibus varius. Nulla facilisis odio enim, eu ullamcorper tellus tristique varius. Pellentesque in posuere diam. Ut sed justo ut nisl pretium posuere ut quis nisl. Nulla nec efficitur metus. Mauris eget nibh orci.</p>
+
+      <p>Sed nec ipsum ultricies, gravida ex sit amet, vulputate dui. Suspendisse potenti. Donec gravida est ut luctus mattis. Mauris quis dolor sapien. Suspendisse volutpat, diam id viverra placerat, dui tortor aliquam ligula, vitae auctor neque nibh quis metus. Ut ultrices sit amet tortor eget egestas. Quisque vel lectus et odio elementum cursus. Nullam id suscipit ipsum, sed vulputate sapien. Vestibulum cursus sem sed enim tincidunt, eget venenatis massa mollis. Vivamus tempor eget leo dignissim accumsan. Nulla in est in leo tempor efficitur id vitae nisl. Duis non nisi sem. Vestibulum consequat, dui et cursus ornare, massa augue posuere diam, sed laoreet tortor tortor vel turpis. Aenean at eros non orci vehicula mollis a sed ante. Fusce mi ante, vulputate nec hendrerit maximus, fringilla ac neque.</p>
+
+      <p>Fusce in mattis libero. Vivamus mi justo, fermentum gravida ultrices sed, tincidunt in sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec viverra justo sed enim mattis blandit. Vivamus facilisis mi ac feugiat vulputate. Nulla eget turpis justo. Fusce molestie, quam quis lobortis sollicitudin, mauris magna elementum leo, at gravida justo mi vel ligula. Cras non ex neque. Sed et risus non enim pharetra mattis. Morbi pellentesque tincidunt justo, nec hendrerit lorem scelerisque sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. In porta ex quis posuere dictum. Mauris ornare at nunc sit amet tempor. In consequat, dui non bibendum volutpat, ante nulla semper nisi, non sollicitudin nisi felis eu urna.</p>
+
+      <p>Vestibulum id fermentum odio. Etiam velit eros, fermentum vel ipsum quis, dignissim viverra diam. Nam ac cursus eros, quis ultricies sapien. Donec et placerat ex. Phasellus vitae viverra mauris. In hac habitasse platea dictumst. Fusce aliquam quam libero, a venenatis magna interdum vitae. Nam quam nibh, efficitur ac cursus et, suscipit non felis. Nam a pretium turpis, sed vehicula augue. Proin dapibus auctor nunc ac semper. Morbi porttitor molestie mattis. Nullam lorem justo, pharetra vel commodo quis, porttitor non nibh. Proin et ex mattis dolor cursus aliquam vitae vitae ipsum.</p>
+
+      <h1 id="about-us">About us</h1>
+      <p>Vivamus arcu libero, egestas in tortor hendrerit, lobortis tincidunt ipsum. Nulla facilisi. Sed vulputate dui id lorem sollicitudin, vitae molestie justo commodo. Ut a laoreet risus. Quisque cursus dictum vestibulum. Donec euismod quam enim, vitae tristique enim iaculis ut. Nullam malesuada felis quis metus lacinia, sit amet imperdiet nisi volutpat. Aliquam erat volutpat. Phasellus odio eros, efficitur vel ipsum a, auctor maximus magna. Praesent in tortor mollis nunc feugiat pulvinar quis quis augue. Aliquam interdum justo vitae felis mollis maximus. Nullam sit amet augue id metus condimentum aliquet.</p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tortor fermentum, egestas sapien vitae, finibus elit. Fusce ullamcorper nec massa vel eleifend. Suspendisse luctus vitae orci sit amet maximus. Donec dictum ligula non dapibus varius. Nulla facilisis odio enim, eu ullamcorper tellus tristique varius. Pellentesque in posuere diam. Ut sed justo ut nisl pretium posuere ut quis nisl. Nulla nec efficitur metus. Mauris eget nibh orci.</p>
+
+      <p>Sed nec ipsum ultricies, gravida ex sit amet, vulputate dui. Suspendisse potenti. Donec gravida est ut luctus mattis. Mauris quis dolor sapien. Suspendisse volutpat, diam id viverra placerat, dui tortor aliquam ligula, vitae auctor neque nibh quis metus. Ut ultrices sit amet tortor eget egestas. Quisque vel lectus et odio elementum cursus. Nullam id suscipit ipsum, sed vulputate sapien. Vestibulum cursus sem sed enim tincidunt, eget venenatis massa mollis. Vivamus tempor eget leo dignissim accumsan. Nulla in est in leo tempor efficitur id vitae nisl. Duis non nisi sem. Vestibulum consequat, dui et cursus ornare, massa augue posuere diam, sed laoreet tortor tortor vel turpis. Aenean at eros non orci vehicula mollis a sed ante. Fusce mi ante, vulputate nec hendrerit maximus, fringilla ac neque.</p>
+
+      <h1 id="contact-us">Contact us</h1>
+      <p>Fusce in mattis libero. Vivamus mi justo, fermentum gravida ultrices sed, tincidunt in sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec viverra justo sed enim mattis blandit. Vivamus facilisis mi ac feugiat vulputate. Nulla eget turpis justo. Fusce molestie, quam quis lobortis sollicitudin, mauris magna elementum leo, at gravida justo mi vel ligula. Cras non ex neque. Sed et risus non enim pharetra mattis. Morbi pellentesque tincidunt justo, nec hendrerit lorem scelerisque sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. In porta ex quis posuere dictum. Mauris ornare at nunc sit amet tempor. In consequat, dui non bibendum volutpat, ante nulla semper nisi, non sollicitudin nisi felis eu urna.</p>
+
+      <p>Vestibulum id fermentum odio. Etiam velit eros, fermentum vel ipsum quis, dignissim viverra diam. Nam ac cursus eros, quis ultricies sapien. Donec et placerat ex. Phasellus vitae viverra mauris. In hac habitasse platea dictumst. Fusce aliquam quam libero, a venenatis magna interdum vitae. Nam quam nibh, efficitur ac cursus et, suscipit non felis. Nam a pretium turpis, sed vehicula augue. Proin dapibus auctor nunc ac semper. Morbi porttitor molestie mattis. Nullam lorem justo, pharetra vel commodo quis, porttitor non nibh. Proin et ex mattis dolor cursus aliquam vitae vitae ipsum.</p>
+
+      <p>Vivamus arcu libero, egestas in tortor hendrerit, lobortis tincidunt ipsum. Nulla facilisi. Sed vulputate dui id lorem sollicitudin, vitae molestie justo commodo. Ut a laoreet risus. Quisque cursus dictum vestibulum. Donec euismod quam enim, vitae tristique enim iaculis ut. Nullam malesuada felis quis metus lacinia, sit amet imperdiet nisi volutpat. Aliquam erat volutpat. Phasellus odio eros, efficitur vel ipsum a, auctor maximus magna. Praesent in tortor mollis nunc feugiat pulvinar quis quis augue. Aliquam interdum justo vitae felis mollis maximus. Nullam sit amet augue id metus condimentum aliquet.</p>
+    </q-page-container>
+
+    <FooterLayout/>
+
+  </q-layout>
+</template>
+
+<script scoped setup lang="ts">
+import { onMounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import HeaderLayout from 'src/components/HeaderLayout.vue';
+import FooterLayout from 'src/components/FooterLayout.vue';
+
+const route = useRoute();
+
+const scrollToSection = (section: string) => {
+  setTimeout(() => {
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, 100); // Small delay to ensure DOM is ready
+};
+
+const handleScroll = () => {
+  const section = route.path.split('/').pop();
+  if (section && section !== 'about') {
+    scrollToSection(section);
+  }
+};
+
+onMounted(() => {
+  handleScroll();
+});
+
+watch(() => route.path, handleScroll);
+</script>
+
+
+
+<style scoped lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  padding: 12px 100px;
+
+  h1 {
+    font-size: 40px;
+    font-weight: bold;
+  }
+}
+</style>
