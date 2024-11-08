1dbae86a6ac13ee8801b4268847c30987b137982
petersartoris <petersartoris1@gmail.com>
2024-11-08 17:47:48 +0100

    seeders continued (needs fix)

--- Backend/adonisrc.ts
+++ Backend/adonisrc.ts
@@ -10,7 +10,11 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
+  commands: [
+    () => import('@adonisjs/core/commands'),
+    () => import('@adonisjs/lucid/commands'),
+    () => import('#commands/run_seeders'),
+  ],
 
   /*
   |--------------------------------------------------------------------------
@@ -30,7 +34,7 @@ export default defineConfig({
     },
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
-    () => import('@adonisjs/auth/auth_provider')
+    () => import('@adonisjs/auth/auth_provider'),
   ],
 
   /*
@@ -41,7 +45,11 @@ export default defineConfig({
   | List of modules to import before starting the application.
   |
   */
-  preloads: [() => import('#start/routes'), () => import('#start/kernel'), () => import('#start/ws')],
+  preloads: [
+    () => import('#start/routes'),
+    () => import('#start/kernel'),
+    () => import('#start/ws'),
+  ],
 
   /*
   |--------------------------------------------------------------------------
