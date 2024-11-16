541f0be513f41926ad8da9b4e0fef57634f6fd62
petersartoris <petersartoris1@gmail.com>
2024-11-16 09:30:11 +0100

    Moved command parser to utils

--- Frontend/src/components/ChatInstance.vue
+++ Frontend/src/components/ChatInstance.vue
@@ -121,8 +121,12 @@ import { ref, computed, nextTick, watch, onMounted } from 'vue';
 import { useMessageStore } from 'src/stores/message_store';
 import { useUserStore } from 'src/stores/user_store';
 import { subscribeToMessages } from 'src/boot/socket';
-import { api } from 'src/boot/axios';
-import axios from 'axios';
+//import { api } from 'src/boot/axios';
+//import axios from 'axios';
+import { useQuasar } from 'quasar';
+import { parseCommand } from 'src/utils/commandParser';
+
+const $q = useQuasar();
 
 const messageStore = useMessageStore();
 const userStore = useUserStore();
@@ -286,179 +290,10 @@ const sendMessage = () => {
   }
 };
 
-import { useQuasar } from 'quasar';
-const $q = useQuasar();
-
 // Command handling - Parser
-const parseCommand = async (command: string) => {
-  const parsedArray = command.split(' ');
-  const action = parsedArray[0];
-  const entityName = parsedArray[1];
-  const channelSettings = parsedArray[2];
-
-  if (!entityName) {
-    $q.notify({
-      position: 'top',
-      type: 'negative',
-      message: 'Please provide a name.',
-    });
-    return;
-  }
-  if (channelSettings && !['private'].includes(channelSettings)) {
-    $q.notify({
-      position: 'top',
-      type: 'negative',
-      message: 'Invalid channel settings.',
-    });
-    return;
-  }
-
-
-  try {
-    let response;
-    switch (action) {
-      // Invite
-      case '/invite':
-        response = await api.post('/send-invite', { userName: entityName, channelName: props.title });
-        $q.notify({
-          position: 'top',
-          type: 'positive',
-          message: response.data.message || 'Invitation sent!',
-        });
-        break;
-      // Revoke
-      case '/revoke':
-        response = await api.post('/revoke-user', { userName: entityName, channelName: props.title });
-        $q.notify({
-          position: 'top',
-          type: 'positive',
-          message: response.data.message || 'User revoked successfully!',
-        });
-        break;
-      // Kick
-      case '/kick':
-        response = await api.post('/kick-user', { userName: entityName, channelName: props.title });
-        $q.notify({
-          position: 'top',
-          type: 'positive',
-          message: response.data.message || 'User kicked successfully!',
-        });
-        break;
-      case '/join':
-        response = await api.post('/join-channel', { channelName: entityName, channelSettings: channelSettings });
-        $q.notify({
-          position: 'top',
-          type: 'positive',
-          message: response.data.message || 'Joined channel successfully!',
-        });
-        break;
-      default:
-
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Invalid command!',
-        });
-    }
-  } catch (error) {
-    // potom mozno zmenit na swtich a zmenit poradie
-    if (axios.isAxiosError(error) && error.response ) {
-      // Already a member
-      if(error.response.status === 400 && error.response.data.message === 'Already a member') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'You are already a member of this channel.',
-        });
-      // Private channel
-      } else if (error.response.status === 403 && error.response.data.message === 'Private channel') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Cannot join a private channel.',
-        });
-      // Channel not found
-      } else if (error.response.status === 404 && error.response.data.message === 'Channel not found') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Channel not found.',
-        });
-      // User not found
-      } else if (error.response.status === 404 && error.response.data.message === 'User not found') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'User not found',
-        });
-      // Invites - Only admin invites (private channel)
-      } else if (error.response.status === 403 && error.response.data.message === 'OnlyAdminInvites') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Only the channel admin can send invites.',
-        });
-      // Invites - User error - not a member
-      } else if (error.response.status === 403 && error.response.data.message === 'You are not a member of this channel') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'You are not a member of this channel',
-        });
-      // Invites - User error - already a member
-      } else if (error.response.status === 400 && error.response.data.message === 'InviteAlreadyMember') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'User is already a member of this channel',
-        });
-      // Revoke - Only admin revokes
-      } else if (error.response.status === 403 && error.response.data.message === 'OnlyAdminRevoke') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Only the channel admin can revoke users.',
-        });
-      // User not in channel
-      } else if (error.response.status === 400 && error.response.data.message === 'UserNotInChannel') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'User is not in this channel.',
-        });
-      // Admin cannot revoke self
-      } else if (error.response.status === 400 && error.response.data.message === 'AdminCannotRevokeSelf') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'Unable to remove user. (Channel Admin)',
-        });
-      } else if (error.response.status === 400 && error.response.data.message === 'UserAlreadyBanned') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'User is already banned from this channel.',
-        });
-      } else if (error.response.status === 403 && error.response.data.message === 'UserBanned') {
-        $q.notify({
-          position: 'top',
-          type: 'negative',
-          message: 'User is banned from the channel.',
-        });
-      }
-    } else {
-      $q.notify({
-        position: 'top',
-        type: 'negative',
-        message: 'An error occurred while processing the commandos.',
-      });
-    }
-  }
-};
-
 const handleMessage = () => {
   if (text.value.startsWith('/')) {
-    parseCommand(text.value);
+    parseCommand(text.value, props, $q);
     text.value = '';
   } else {
     sendMessage();
